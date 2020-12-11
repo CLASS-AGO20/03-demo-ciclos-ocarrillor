aff489bf7749fe725f4e42d4f46efada166dc8ba
ocarrillor <ocarrillo7@ucol.mx>
2020-12-10 19:54:45 -0600

    Implemented function sumarParesFor

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-export default class App {}
+export default class App {
+    sumarParesFor(){
+        let suma = 0;
+        for(let i = 1; i <= 20; i++){
+            if(i % 2 == 0){
+                suma = suma + i;
+            }
+        }
+        return suma;
+    }
+}
 
 let app = new App();
+
+//Demostracion de la funcion sumarParesFor.
+console.log(app.sumarParesFor());
